d1f6df3a549d75d68f21e868e34307be1561396e
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-06-30 15:20:46 +0100

    update

--- src/20-GameOfLife.elm
+++ src/20-GameOfLife.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (beginnerProgram, div, span, button, text, node)
-import Html.Attributes exposing (rel, href)
+import Html.Attributes exposing (class, rel, style, href, classList)
 import Html.Events exposing (onClick)
+import Dict
 
 
 main =
@@ -10,30 +11,66 @@ main =
 
 
 model =
-    0
+    Dict.empty
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Tick
+    | Toggle ( Int, Int )
 
 
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        Tick ->
+            Dict.empty
 
-        Decrement ->
-            model - 1
+        Toggle ( row, column ) ->
+            Dict.update ( row, column )
+                (\isAlive ->
+                    case isAlive of
+                        Nothing ->
+                            Just True
+
+                        Just True ->
+                            Nothing
+
+                        Just False ->
+                            Just True
+                )
+                model
 
 
+n : number
 n =
     10
 
 
+cellSize =
+    20
+
+
 view model =
     div []
-        (List.range 0 (n * n - 1)
-            |> List.map (\i -> ( i // n, i % n ))
-            |> List.map (\( i, j ) -> div [] [ text (toString i ++ "-" ++ toString j) ])
-        )
+        [ node "link" [ rel "stylesheet", href "style.css" ] []
+        , span [] [ text (toString model) ]
+        , div [ style [ ( "width", toString (cellSize * n) ++ "px" ), ( "height", toString (cellSize * n) ++ "px" ) ] ]
+            (List.range 0 (n * n - 1)
+                |> List.map (\i -> ( i // n, i % n ))
+                |> List.map (\( row, column ) -> ( row, column, Dict.member ( row, column ) model ))
+                |> List.map
+                    (\( i, j, isAlive ) ->
+                        div
+                            [ style
+                                [ ( "top", toString (cellSize * i) )
+                                , ( "left", toString (cellSize * j) )
+                                , ( "width", "20px" )
+                                , ( "height", "20px" )
+                                ]
+                            , classList [ ( "cell", True ), ( "alive", isAlive ) ]
+                            , onClick (Toggle ( i, j ))
+                            ]
+                            []
+                    )
+            )
+        , button [ onClick Tick ] [ text "Tick" ]
+        ]
